44a68d04607c5d9feb7bdc840d966adbd7128587
jersson <jerssonsanchez2007@gmail.com>
2025-04-05 15:05:09 -0500

    se agregaron cambios

--- ejercicio3.js
+++ ejercicio3.js
@@ -16,13 +16,22 @@ class CuentaBancaria {
                 console.log("el monto de deposito debe ser positivo");
                 };
     }
-    this.retirar = fuction(monto){
+    this.retirar = function(monto){
         if (monto > 0 && monto <= saldo) {
             saldo -= monto;
             console.log(`se han retirado $${monto}. Saldo actual: $${saldo}`);
         } else{
-            
+            console.log("fondos insuficientes o monto invalido.");
         }
-    }
+    };
   }
 }
+
+const cuenta1 = new CuentaBancaria(1000);
+ console.log("saldo inicial:", cuenta1.getSaldo());
+ 
+ cuenta1.depositar(500);
+ cuenta1.retirar(200);
+
+ console.log("saldo final:", cuenta1.getSaldo());
+ 
